56ad6150ab0675e0dfdc942b0b8b70a56d2ce560
= <=>
2022-10-29 12:57:13 +0400

    Add project files.

---
+++ 01-Intro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string categoryName = "Notebooks";
+            Console.WriteLine(categoryName);
+
+            int initiAlCount = 0;
+            double dolarToday = 7.45;
+            decimal unitPrice = 1200;
+            var userName = "Jhone";
+            var increase = 10;
+            bool isLoading = true;
+
+            if (isLoading)
+            {
+                Console.WriteLine("Please wait...");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome : {userName}");
+            }
+        }
+    }
+}
