e2333897c80fe4795a5d9c12179c6e9bc12fae01
= <=>
2022-10-30 11:05:31 +0400

    completed collections

---
+++ 06-Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> courses = new List<string> { "HTML","CSS","JS","REACT","REDUX=TOOLKIT" };
+
+            courses.Add("NEXT JS");
+            courses.Remove("CSS");
+            Console.WriteLine($"Total course : {courses.Count}");
+
+            foreach (string course in courses)
+            {
+                Console.WriteLine(course);
+            }
+
+            //courses.Add("JEQUERY");
+            
+        }
+    }
+}
